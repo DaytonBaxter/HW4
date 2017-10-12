80db43f07a317f004f596cf21a226cc2a70de438
abigailsturgeon <31452283+abigailsturgeon@users.noreply.github.com>
2017-10-12 13:09:43 -0600

    House input and display

--- sodium_hw4_task1.cpp
+++ sodium_hw4_task1.cpp
@@ -3,103 +3,62 @@
 // Copyright (c) 2017 WSU
 
 #include <iostream>
+#include <string>
 using namespace std;
 
-// Structures/Constants
-struct house
+const int SIZE = 3;
+
+class houseInfo
 {
+private:
     int streetNum;
-    char streetName[30];
+    string streetName;
     double price;
+public:
+    void houseInput()
+    {
+        cout << "Please enter the street number: " << endl;
+        cin >> streetNum;
+
+        cout << "Please enter the street name: " << endl;
+        cin >> streetName;
+
+        cout << "Please enter the price: " << endl;
+        cin >> price;
+    }
+    void showHouse(int houseNum)
+    {
+        cout << houseNum << " house at " << streetNum
+             << " " << streetName << " for $" << price << ".00." << endl;
+    }
+    double returnPrice()
+    {
+        return price;
+    }
+
 };
 
-//   Function prototypes
-struct house InputHouse(void);
-void PrintHouse(int houseNum, struct house h);
-int ComparePrices(struct house h1, struct house h2);
-
-//   Main Program
-int main(int argc, char* argv[])
+int main()
 {
-    cout<<"Enter info for first house\n";
-    struct house firstHouse =  InputHouse();
-    cout<<"\nEnter info for second house\n";
-    struct house secondHouse = InputHouse();
+    houseInfo array[SIZE];
+    houseInfo h1, h2, h3;
+    cout << "Enter info for 1 house: " << endl;
+    h1.houseInput();
+    array[0] = h1;
 
-    PrintHouse(1, firstHouse);
-    printf("\n\n");
-    PrintHouse(2, secondHouse);
-    printf("\n\n");
+    cout << "Enter info for 2 house: " << endl;
+    h2.houseInput();
+    array[1] = h2;
 
-    int comparison = ComparePrices(firstHouse, secondHouse);
-    /*   Based on comparison value, use a switch
-     *  statement to display all possibilities:
-     *  1) "the first house is cheaper"
-     *  2) "the two houses are equally expensive"
-     *  3) "the second house is cheaper"
-     *  4) "invalid comparison value there must be a bug"
-     */
+    cout << "Enter info for 3 house: " << endl;
+    h3.houseInput();
+    array[2] = h3;
 
-    // Depending on returned integer, print out results
-    switch(comparison)
-    {
-        case 1:
-            cout << "The first house is cheaper." << endl;
-            break;
-        case 2:
-            cout << "The house are equally expensive." << endl;
-            break;
-        case 3:
-            cout << "The second house is cheaper." << endl;
-            break;
-        default:
-            cout << "Unsupported code" << endl;
-            break;
-    }
+    cout << "Your market analysis is as follows: " << endl;
+    array[0].showHouse(1);
+    array[1].showHouse(2);
+    array[2].showHouse(3);
+
+
     return 0;
-}
-
-//   Function Definitions
-
-// Get addresses and prices for houses from user
-struct house InputHouse(void)
-{
-    house h;
-    cout << "Please enter the street number: " << endl;
-    cin >> h.streetNum;
-
-    cout << "Please enter the street name: " << endl;
-    cin >> h.streetName;
-
-    cout << "Please enter the price: " << endl;
-    cin >> h.price;
-
-    return h;
-}
-
-// Print out house info
-void PrintHouse(int houseNum, struct house h)
-{
-    cout << houseNum << " house at " << h.streetNum
-         << " " << h.streetName << " for $" << h.price << ".00." << endl;
-}
-
-// Compare house prices and return comparison integer
-int ComparePrices(struct house h1, struct house h2)
-{
-    int i = 0;
-    if(h1.price < h2.price)
-    {
-        i = 1;
-    }
-    else if(h1.price == h2.price)
-    {
-        i = 2;
-    }
-    else if(h1.price > h2.price)
-    {
-        i = 3;
-    } else (i = 4);
-    return i;
-
 }
